229c90298073e81f14cb14f7d5d9cb3673ac2938
jbenastey <jbenastey@gmail.com>
2019-09-29 18:05:10 +0700

    tambah title dan icon

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>AdminLTE 3 | Dashboard</title>
+	<title>Pustaka Wilayah</title>
 	<!-- Tell the browser to be responsive to screen width -->
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Font Awesome -->
@@ -27,6 +27,7 @@
 	<link rel="stylesheet" href="<?=base_url()?>assets/plugins/datatables/dataTables.bootstrap4.min.css">
 	<!-- Google Font: Source Sans Pro -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+	<link rel="shortcut icon" href="<?=base_url()?>assets/dist/img/iconBuku2.png">
 </head>
 <body class="hold-transition sidebar-mini sidebar-collapse">
 <div class="wrapper">
